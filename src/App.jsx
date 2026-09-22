4d47c71d3d7abdce8210e196cf66c7afedaaa9e0
Sam Verneuille <samvernel268@gmail.com>
2026-09-21 21:20:52 -0400

    fix CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import heroImg from "./assets/hero.png";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
-import "./App.css";
+import "./Todo.css";
 import Todo from './components/Todo.jsx';
 import Title from './components/Title.jsx';
 
